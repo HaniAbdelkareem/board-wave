af86359c6ce5bab61fe7dc0793de42533cdff3f5
Hani Abdelkareem <hanim1033@gmail.com>
2024-08-03 00:54:26 +0300

    Rendering the user created boards in the dashboard

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -3,9 +3,11 @@
 import { useQuery } from "convex/react"
 import { api } from "@/convex/_generated/api"
 
+import { BoardCard } from "./board-card"
 import { EmptyBoards } from "./empty-boards"
 import { EmptyFavorites } from "./empty-favorites"
 import { EmptySearch } from "./empty-search"
+import { NewBoardButton } from "./new-board-button"
 
 interface BoardListProps {
   orgId: string
@@ -21,7 +23,16 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   if (data === undefined) {
     return (
       <div>
-        Loading...
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite boards" : "Team boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled/>
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
       </div>
     )
   }
@@ -44,6 +55,22 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
       <h2 className="text-3xl">
         {query.favorites ? "Favorite boards" : "Team boards"}
       </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
     </div>
   )
 }
