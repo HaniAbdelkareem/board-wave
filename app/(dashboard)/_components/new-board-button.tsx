94a1fc174559ab64efaf17f23f9802653f81238f
Hani Abdelkareem <hanim1033@gmail.com>
2024-08-04 21:39:52 +0300

    Adding search query + the skeleton for the canvas

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -2,6 +2,7 @@
 
 import { toast } from "sonner"
 import { Plus } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 import { cn } from "@/lib/utils"
 import { api } from "@/convex/_generated/api"
@@ -13,6 +14,7 @@ interface NewBoardButtonProps {
 }
 
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const router = useRouter()
   const { mutate, pending } = useApiMutation(api.board.create)
   const onClick = () => {
     mutate({
@@ -21,7 +23,7 @@ export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
     })
       .then((id) => {
         toast.success("Board created")
-        // TODO: Redirect to board/{id}
+        router.push(`/board/${id}`)
       })
       .catch(() => toast.error("Failed to create board"))
   }
